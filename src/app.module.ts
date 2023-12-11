545760c69b615730249e07b0e9b5f5be284af670
IlyasBajtit <ilyas.bajtit@gmail.com>
2023-12-11 05:06:17 +0100

    feat: add res borrows

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BorrowsModule } from './borrows/borrows.module';
 
 @Module({
-  imports: [],
+  imports: [BorrowsModule],
   controllers: [AppController],
   providers: [AppService],
 })
