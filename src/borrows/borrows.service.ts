7d89c075b3b16a1fcc7fb813a0eb8108ac0b8551
aguitinan <aguit.inan@gmail.com>
2023-12-15 14:06:46 +0100

    feat: update borrows.service.ts

--- src/borrows/borrows.service.ts
+++ src/borrows/borrows.service.ts
@@ -16,6 +16,45 @@ export class BorrowsService {
     return new NormalizedResponse(`Borrow with UUID ${uuid} found`, borrowedItem).toJSON();
   }
 
+  public async getByBorrowUUID(uuid: string) {
+    return new NormalizedResponse(
+      `Borrow for '${uuid}' uuid has been found`,
+      await this.prisma.borrows.findUnique({
+        where: {
+          borrow_UUID: uuid,
+        },
+      }),
+    ).toJSON();
+  }
+
+  public async getByEmployeeUUID(uuid: string) {
+    return new NormalizedResponse(
+      `Borrow for '${uuid}' uuid has been found`,
+      await this.prisma.employees.findUnique({
+        where: {
+          employee_UUID: uuid,
+        },
+        include: {
+          borrows: true,
+        }
+      }),
+    ).toJSON();
+  }
+
+  public async getByBorrowerUUID(uuid: string) {
+    return new NormalizedResponse(
+      `Borrow for '${uuid}' uuid has been found`,
+      await this.prisma.borrowers.findUnique({
+        where: {
+          borrower_UUID: uuid,
+        },
+        include: {
+          borrows: true,
+        }
+      }),
+    ).toJSON();
+  }
+
   public async create(createBorrowDto: CreateBorrowDto) {
     const createdBorrow = await this.prisma.borrows.create ({
         data: {
